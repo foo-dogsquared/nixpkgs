3d6b0aa1986df51dfce9fa703df3d48360c07e95
Gabriel Arazas <foo.dogsquared@gmail.com>
2020-09-08 14:16:38 +0800

    brl-cad: init at 7.30.10

---
+++ pkgs/applications/graphics/brl-cad/default.nix
@@ -0,0 +1,51 @@
+{ clangStdenv,
+  cmake,
+  fetchsvn,
+  libGL,
+  libXi,
+  libXft,
+  ncurses
+}:
+
+clangStdenv.mkDerivation rec {
+  pname = "brl-cad";
+  version = "7.30.10";
+ 
+  src = let
+    repo = "brlcad";
+    tag = builtins.replaceStrings ["."] ["-"] version;
+    revision = "77067";
+    in fetchsvn {
+      url = "svn://svn.code.sf.net/p/${repo}/code/${repo}/tags/rel-${tag}";
+      rev = revision;
+      sha256 = "1wmx1mr2a2zk20qx1inngsi0d4cfvfaqdzb9k84ijzrkil2zbs7d";
+  };
+
+  nativeBuildInputs = [ cmake ];
+
+  buildInputs = [
+    libGL
+    libXft
+    libXi
+    ncurses
+  ];
+
+  cmakeFlags = [
+    "-DBRLCAD_ENABLE_STRICT=OFF"
+    "-DBRLCAD_BUNDLED_LIBS=ON"
+    "-DCMAKE_BUILD_TYPE=Release"
+  ];
+
+  enableParallelBuilding = true;
+
+  meta = with clangStdenv.lib; {
+    description = "A free and open-source 3D solid modelling system.";
+    homepage = "http://www.brlcad.org/";
+    license = licenses.free;
+    maintainers = with maintainers; [ foo-dogsquared ];
+    platforms = platforms.linux
+      ++ platforms.freebsd
+      ++ platforms.netbsd
+      ++ platforms.openbsd;
+  };
+}
